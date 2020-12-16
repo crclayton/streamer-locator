e890eca200a1c3e6ceb4bdfd7371fb4def8ea268
Charles Clayton <charlesrussellclayton@gmail.com>
2020-12-16 09:18:03 -0800

    Update python-wireshark-geolocate.py

--- python-wireshark-geolocate.py
+++ python-wireshark-geolocate.py
@@ -4,6 +4,8 @@ from geolite2 import geolite2
 import socket, subprocess 
 
 cmd = r"C:\Program Files\Wireshark\tshark.exe"
+# if ethernet try
+cmd = r"C:\Program Files\Wireshark\tshark.exe -i ethernet"
 
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 my_ip = socket.gethostbyname(socket.gethostname())
@@ -33,9 +35,16 @@ def get_ip_location(ip):
 for line in iter(process.stdout.readline, b""):
     columns = str(line).split(" ")
 
-    if "->" in columns and ("SKYPE" in columns or "UDP" in columns):
-        src_ip = columns[columns.index("->") - 1]
-
+    if "SKYPE" in columns or "UDP" in columns:
+        
+        # for different tshark versions
+        if "->" in columns:
+            src_ip = columns[columns.index("->") - 1]
+        elif "\\xe2\\x86\\x92" in columns:
+            src_ip = columns[columns.index("\\xe2\\x86\\x92") - 1]
+        else:
+            continue
+            
         if src_ip == my_ip:
             continue
 
